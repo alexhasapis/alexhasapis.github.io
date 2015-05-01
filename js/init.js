cd0eea807a5d3ba4dbaa9787fd4983ee2cfa507d
Alex Hasapis <alexmhasapis@gmail.com>
2015-05-01 18:59:56 -0400

    modal body scroll fix

--- js/init.js
+++ js/init.js
@@ -3,9 +3,13 @@
 
     $('.button-collapse').sideNav();
 
-    $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal({
+      ready: function() { $("body").addClass("modal-open"); },
+      complete: function() { $("body").removeClass("modal-open"); }
+    });
 
     $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
